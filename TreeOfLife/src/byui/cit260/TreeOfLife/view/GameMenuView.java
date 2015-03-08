20330e467a28c47ed4664657d57c2a28647181f5
Andrew <Andrew@AndrewLaptop>
2015-03-08 00:05:49 -0700

    Menus

--- TreeOfLife/src/byui/cit260/TreeOfLife/view/GameMenuView.java
+++ TreeOfLife/src/byui/cit260/TreeOfLife/view/GameMenuView.java
@@ -9,11 +9,55 @@ package byui.cit260.TreeOfLife.view;
  *
  * @author Andrew
  */
-public class GameMenuView {
+public class GameMenuView extends View {
 
-    void displayMenu() {
-        System.out.println("\n*** displayMenu stub function called ***");
+    public String gameMenu = "";    
+    public GameMenuView(){
+    super("\n"
+            + "\n-----------------------------------------"
+            + "\n Game Menu"
+            + "\n-----------------------------------------"
+            + "\nR - Resume" 
+            + "\nS - Save" 
+            + "\nA - Go to armor shop"
+            + "\nE - Exit"
+            + "\n-----------------------------------------");
+    }
+    
+    
+ @Override
+    public void doAction(Object obj) {
         
+        char value = (char) obj;
+        
+        switch (value){
+            case 'R': // Resume Game
+                this.resumeGame();
+                break;
+            case 'S': // Save
+                this.saveGame();
+                break;
+            case 'A': // Go to Armor Shop
+                this.displayAddArmorMenu();
+                break;
+            case 'E': // Exit
+                return;
+            default:
+                System.out.println("\n*** Invalid Selection ***");
+                break;
+        }
+    }
+    
+    private void saveGame() {
+       System.out.println("*** saveGame function called ***");
+    }
+
+    private void resumeGame() {
+        System.out.println("*** resumeGame function called ***");
+    }
+
+    private void displayAddArmorMenu() {
+        System.out.println("*** displayAddArmorMenu function called ***");
     }
     
 }
